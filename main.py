8a5f78dfea2252b2f14abaf03373132bdbd6acb5
deep <shahdeep1908@gmail.com>
2022-11-23 10:46:35 +0530

    Created basic structure and logic for check sequence of data. Tested Successfully.

--- main.py
+++ main.py
@@ -3,12 +3,26 @@ import unicodedata
 
 import requests
 from bs4 import BeautifulSoup
+from difflib import SequenceMatcher
 
 headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:48.0) Gecko/20100101 Firefox/48.0'}
 
 
-def check_sequence_of_data():
+def mismatch():
+    """
+    Set the difference for Minor Mismatch with two strings.
+    """
+    # SequenceMatcher(a=a, b=b).ratio()
     pass
+
+
+def check_sequence_of_data(blog_txt_lst, jidipi_txt_lst):
+    for jidipi_str, blog_str in zip(jidipi_txt_lst, blog_txt_lst):
+        if jidipi_str == blog_str:
+            print(True)
+        else:
+            print(False)
+    return
 
 
 def get_blog_url_data(url):
@@ -34,27 +48,32 @@ def get_jidipi_url_data(url):
             parent_tag = parent_tag.find("p").parent
             flag = False
             for check_element in parent_tag.contents:
-                if check_element.find("strong"):
+                if check_element.find("strong") or check_element.find("h4"):
                     flag = True
                     continue
             if not flag:
                 cleaned_data = paragraph_tag.text.strip()
+                print(cleaned_data, len(cleaned_data), ':::')
                 if cleaned_data not in jidipi_result:
                     jidipi_result.append(cleaned_data)
 
     return jidipi_result
 
 
-blog_url = "https://www.archdaily.com/938054/chinese-culture-exhibition-center-qingdao-tengyuan-design-plus-eca2"
-jidipi_url = "https://architectures.jidipi.com/j00063373/en/chinese-culture-exhibition-center"
+# blog_url = "https://www.archdaily.com/938054/chinese-culture-exhibition-center-qingdao-tengyuan-design-plus-eca2"
+blog_url = "https://www.archdaily.com/246905/villa-jian-cu-office?ad_source=search&ad_medium=projects_tab"
+# jidipi_url = "https://architectures.jidipi.com/j00063373/en/chinese-culture-exhibition-center"
+jidipi_url = "https://architectures.jidipi.com/j00030499/en/villa-jian"
 
 blog = get_blog_url_data(blog_url)
 jidipi = get_jidipi_url_data(jidipi_url)
 
-with open('blog.txt', 'w') as f:
-    # f.write(re.sub(r'(?<=[.,])(?=[^\s])', r' ', ''.join(blog)))
-    f.write(re.sub(r'(?<=[.,])(?=[^\s])', r' ', unicodedata.normalize("NFKD", ''.join(blog))))
+blog_txt = re.sub(r'(?<=[.,])(?=[^\s])', r' ', unicodedata.normalize("NFKD", ''.join(blog)))
+jidipi_txt = re.sub(r'(?<=[.,])(?=[^\s])', r' ', unicodedata.normalize("NFKD", ''.join(jidipi)))
 
-with open('jidipi.txt', 'w') as f:
-    # f.write(re.sub(r'(?<=[.,])(?=[^\s])', r' ', ''.join(jidipi)))
-    f.write(re.sub(r'(?<=[.,])(?=[^\s])', r' ', unicodedata.normalize("NFKD", ''.join(jidipi))))
+# print(blog_txt.split('.'))
+# print(len(blog_txt.split('.')))
+# print()
+# print(jidipi_txt.split('.'))
+# print(len(jidipi_txt.split('.')))
+check_sequence_of_data(blog_txt.split('.'), jidipi_txt.split('.'))
