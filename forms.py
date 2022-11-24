73117064927d5a45094c0f86265d72cbb2564624
deep <shahdeep1908@gmail.com>
2022-11-24 19:17:10 +0530

    forms for validating input

--- forms.py
+++ forms.py
@@ -1,8 +1,23 @@
 from flask_wtf import FlaskForm
-from wtforms import URLField
+from wtforms import URLField, DecimalField
 from wtforms.validators import InputRequired
 
 
 class BlogForm(FlaskForm):
+    """
+    Form for URL Inputs.
+    blog_url: URL from where the data comes.
+    jidipi_url: URL for which data needs to be checked.
+    """
     blog_url = URLField('blog_url', validators=[InputRequired()])
     jidipi_url = URLField('jidipi_url', validators=[InputRequired()])
+
+
+class SplitDataForm(FlaskForm):
+    """
+    Form for Slicing Data Inputs
+    blog_url: URL from where the data comes.
+    data_split_num: No of chunks required
+    """
+    blog_url = URLField('blog_url', validators=[InputRequired()])
+    data_split_num = DecimalField('data_split_num', validators=[InputRequired()])
