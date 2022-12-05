73eed950e0f039fe7f264ec6ad9fffbac62c2d1b
shahdeep-tech <shahdeep1908@gmail.com>
2022-12-05 16:02:29 +0530

    Added count of extras dot function to it.

---
+++ utils/extras.py
@@ -0,0 +1,16 @@
+import re
+
+
+def check_extra_dots(blog):
+    num_of_dots = {'dot_count_2': 0, 'dot_count_3': 0, 'dot_count_4': 0}
+    for dots in blog:
+        regex = re.split(r'(\.+)', dots)
+        num_of_dots['dot_count_2'] += regex.count('..')
+        num_of_dots['dot_count_3'] += regex.count('...')
+        num_of_dots['dot_count_4'] += regex.count('....')
+    print('*' * 50)
+    print('dot_count_2 :', num_of_dots['dot_count_2'])
+    print('dot_count_3 :', num_of_dots['dot_count_3'])
+    print('dot_count_4 :', num_of_dots['dot_count_4'])
+    print('*' * 50)
+    return num_of_dots
