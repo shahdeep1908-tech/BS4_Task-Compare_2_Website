cdabef78a1c2b35fb8ba62ff11a7bc6664e7fa53
shahdeep-tech <shahdeep1908@gmail.com>
2022-12-06 10:29:54 +0530

    Added Data division view and template

--- main.py
+++ main.py
@@ -38,5 +38,21 @@ def split_data():
     return render_template('split_blog_data.html', form=form)
 
 
+@app.route('/split-data_by_parts', methods=['POST', 'GET'])
+def split_data_by_parts():
+    """
+    Split data into chunks
+
+    :return: Template
+    """
+    form = SplitDataForm()
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            status, data_lst, max_data = split_url_data.get_data_to_split(form.blog_url.data, form.data_split_num.data)
+            return render_template('split_blog_data.html', form=form, status=status, data_lst=data_lst,
+                                   max_data=max_data)
+    return render_template('split_blog_data.html', form=form)
+
+
 if __name__ == '__main__':
     app.run(debug=True)
